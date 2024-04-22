7839058c0d904858dd03214fe8e7869fd52cf94a
Bekry <bekrye@gmail.com>
2024-04-22 16:28:12 +0200

    Complete Endpoints and add Cors config

--- src/main/kotlin/com/example/backendtaskapp/controller/TaskController.kt
+++ src/main/kotlin/com/example/backendtaskapp/controller/TaskController.kt
@@ -1,10 +1,18 @@
 package com.example.backendtaskapp.controller
 
+import com.example.backendtaskapp.data.model.TaskCreateRequest
 import com.example.backendtaskapp.data.model.TaskDto
+import com.example.backendtaskapp.data.model.TaskUpdateRequest
 import com.example.backendtaskapp.service.TaskService
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,4 +32,19 @@ class TaskController(private val service: TaskService) {
     @GetMapping("task/{id}")
     fun getTaskById(@PathVariable id: Long): ResponseEntity<TaskDto> =
         ResponseEntity(service.getTaskById(id), HttpStatus.OK)
+
+    @PostMapping("create")
+    fun createTask(
+        @Valid @RequestBody request: TaskCreateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.createTask(request), HttpStatus.OK)
+
+    @PatchMapping("update/{id}")
+    fun updateTask(
+        @PathVariable id: Long,
+        @Valid @RequestBody request: TaskUpdateRequest
+    ): ResponseEntity<TaskDto> = ResponseEntity(service.updateTask(id, request), HttpStatus.OK)
+
+    @DeleteMapping("delete/{id}")
+    fun deleteTask(@PathVariable id: Long): ResponseEntity<String> =
+        ResponseEntity(service.deleteTask(id), HttpStatus.OK)
 }
