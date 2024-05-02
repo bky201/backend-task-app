d29c728d693024820f5a73a40ec2b4a8b113bb5a
Bekry <bekrye@gmail.com>
2024-05-02 19:05:06 +0200

    Unit test for Service Task

---
+++ src/test/kotlin/com/example/backendtaskapp/repository/TaskRepositoryEmbeddedTest.kt
@@ -0,0 +1,80 @@
+package com.example.backendtaskapp.repository
+
+import com.example.backendtaskapp.data.Task
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.jdbc.Sql
+
+@DataJpaTest(properties = ["spring.jpa.properties.javax.persistence.validation.mode=none"])
+class TaskRepositoryEmbeddedTest {
+
+    @Autowired
+    private lateinit var objectUnderTest: TaskRepository
+
+    private val numberOfRecordsInTestDataSql = 3
+    private val numberOfOpenRecordsInTestDataSql = 1
+    private val numberOfClosedRecordsInTestDataSql = 2
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when task is saved then check for not null`() {
+        // Given
+        // When
+        val task: Task = this.objectUnderTest
+            .findTaskById(111)
+
+        //Then
+        assertThat(task).isNotNull
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when all tasks are fetched then check for the number of records`() {
+        // Given
+        // When
+        val tasks: List<Task> = objectUnderTest.findAll()
+
+        // Then
+        assertThat(tasks.size).isEqualTo(numberOfRecordsInTestDataSql)
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when task is deleted then check for the size of list`() {
+        // Given
+        // When
+        objectUnderTest.deleteById(113)
+        val tasks: List<Task> = objectUnderTest.findAll()
+
+        // Then
+        assertThat(tasks.size).isEqualTo(2)
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when all open tasks are queried then check for the correct number of open tasks`() {
+        val tasks: List<Task> = objectUnderTest.queryAllOpenTasks()
+
+        assertThat(tasks.size).isEqualTo(numberOfOpenRecordsInTestDataSql)
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when all closed tasks are queried then check for the correct number of closed tasks`() {
+        val tasks: List<Task> = objectUnderTest.queryAllClosedTasks()
+
+        assertThat(tasks.size).isEqualTo(numberOfClosedRecordsInTestDataSql)
+    }
+
+    @Test
+    @Sql("classpath:test-data.sql")
+    fun `when description is queried then check if description already exists`() {
+        val doesDescriptionExist1 = objectUnderTest.doesDescriptionExist("second test todo")
+        val doesDescriptionExist2 = objectUnderTest.doesDescriptionExist("take care the birds")
+
+        assertThat(doesDescriptionExist1).isTrue
+        assertThat(doesDescriptionExist2).isFalse
+    }
+}
